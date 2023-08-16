dd95a4725d570719ce5d6a984a3cd844b8c9e2a7
Abdurrahman Farras Fadhila <farrasfadhila76@gmail.com>
2023-08-16 10:28:09 +0700

    New Layout

--- js/Home.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed('#element', {
-    strings: ['esigner', 'eveloper'],
-    typeSpeed: 100,
-    backSpeed: 40,
-    smartBackspace: false,
-    loop: true,
-    showCursor: true,
-    onLastStringBackspaced: (self) => {return "A"},
-});
---
+++ js/main.js
@@ -0,0 +1,13 @@
+function script(pages){
+    if(pages == "Home"){
+        var typed = new Typed('#element', {
+            strings: ['esigner', 'eveloper'],
+            typeSpeed: 100,
+            backSpeed: 40,
+            smartBackspace: false,
+            loop: true,
+            showCursor: true,
+            onLastStringBackspaced: (self) => {return "A"},
+        });
+    }
+}
